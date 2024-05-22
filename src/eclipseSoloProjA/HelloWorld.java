c88c22c3591c8bd173796f66ebf0b863ee79b926
cegeh <cegeh@10.0.0.81>
2024-05-22 12:52:26 -0400

    commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,10 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hi Github project");
-		<<<<<<< HEAD
+		
 		System.out.println("Programmer 2 did this");
-	=======
-		System.out.println("ProgrammerOne did this");
-	>>>>>>> branch 'master' of https://github.com/cegehPoke/eclipseSU24.git;
+        System.out.println("ProgrammerOne did this");
+
 	}
 }
